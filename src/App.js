3472c8f346df0a2c0d5460c72fe630a9c77b11bb
Leticia <leticia.bora@gmail.com>
2020-11-02 10:57:57 -0300

    lint test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './trivia.png';
 import './App.css';
 
-export default function App() {
+export default function App(){
   return (
     <div className="App">
       <header className="App-header">
